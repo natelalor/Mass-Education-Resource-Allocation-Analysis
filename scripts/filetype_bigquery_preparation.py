ca5617ad1b2ed76141638ed67a4fa8cc2bd99065
Nate Lalor <natelalor@gmail.com>
2026-05-18 10:21:34 -0400

    python filetype converter 1.0

--- scripts/filetype_bigquery_preparation.py
+++ scripts/filetype_bigquery_preparation.py
@@ -1,3 +1,16 @@
-# python program to convert .xlsx files to .csv which are supported by bigquery for further SQL queries
+# May 18, 2026
+# Python script using Pandas library to convert the xlsx filetypes within given directory to csv files so they are supported filetypes for BigQuery SQL query analysis
 
-import pandas
+import pandas as pd
+from pathlib import Path
+
+# outline file I/O pathing
+input_file_path = Path("./data/raw_xlsx")
+output_file_path = Path("./data/raw_csv")
+output_file_path.mkdir(exist_ok=True)
+
+# for each xlsx file in directory, convert to csv file
+for xlsx_file in input_file_path.glob("*.xlsx"):
+    temp_dataframe = pd.read_excel(xlsx_file)
+    temp_dataframe.to_csv(output_file_path / f"{xlsx_file.stem}.csv", index=False)
+    print(f"Converted: {xlsx_file.name}")
