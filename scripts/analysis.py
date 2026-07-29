ecd16c70a431612cc3236155e3d2b6cb5c76aea5
Nate Lalor <natelalor@gmail.com>
2026-07-29 14:25:20 -0400

    cleaned csv data import, prepping python cleaning file

---
+++ scripts/analysis.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+from pathlib import Path
+
+# pandas analysis now that data is cleaned in Snowflake and exported to csv files
+input_file_path = Path("./data/raw_csv")
+
+
